e185e142d74f5c79642a14f768dc2f1368f39c0c
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2024-07-27 23:14:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
 
+
 const allSongs = [
   {
     id: 0,

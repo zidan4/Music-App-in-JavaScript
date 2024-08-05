636d2054d031c9afb6d7dbe30abdb239360aaf6d
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2024-08-05 21:38:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const playlistSongs = document.getElementById("playlist-songs");
-
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
 const nextButton = document.getElementById("next");
